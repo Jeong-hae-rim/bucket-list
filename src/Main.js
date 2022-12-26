ccda2bfbccba25a69a21546daf33f89bac674cbb
정해림 <maestro@haerimjeong.local>
2022-12-26 21:49:17 +0900

    back-ground and coffee animation

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,15 @@
+import { Coffee } from "./components/Coffee";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 
 
 function App() {
   return (
-    <></>
+    <>
+      <Header/>
+      <Coffee/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+
+export const Footer = () => {
+    return (
+        <footer>
+            Made by Hearim
+        </footer>
+    )
+}
