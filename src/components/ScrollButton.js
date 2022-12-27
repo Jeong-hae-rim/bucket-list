e7a44234a3d0ac64835665405ff473aaa15e7915
정해림 <maestro@haerimjeong.local>
2022-12-28 02:01:09 +0900

    scroll button 모양 및 애니메이션 구현

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -1,3 +1,56 @@
+import styled from "styled-components"
+
+const Scroll = styled.div`
+    width: 2rem;
+    height: 3rem;
+    border: 2px solid white;
+    border-radius: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ScrollWheele = styled.button`
+    width: 1rem;
+    height: 1rem;
+    border: none;
+    border-radius: 50%;
+    background: white;
+    display: block;
+    animation: cursor 1.5s linear infinite;
+
+    @keyframes cursor {
+        0% {
+            opacity: 0;
+            transform: translateY(-0.5rem);
+        }
+
+        100% {
+            opacity: 1;
+            transform: translateY(0.5rem);
+        }
+    }
+`;
+
+const ArrowDown = styled.div`
+    width: 1rem;
+    height: 1rem;
+    border: 2px solid transparent;
+    border-right-color: white;
+    border-bottom-color: white;
+    transform: rotate(45deg);
+    margin-top: 0.3rem;
+    animation : arrow 1.5s linear infinite;
+
+    @keyframes arrow {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+`;
 
 
 const ScrollButton = ({yCoord}) => {
@@ -11,7 +64,10 @@ const ScrollButton = ({yCoord}) => {
 
     return(
         <div className="scroll-button">
-            <button onClick={handleScroll}>클릭</button>
+            <Scroll>
+                <ScrollWheele onClick={handleScroll} />
+            </Scroll>
+            <ArrowDown />
         </div>
     )
 }
